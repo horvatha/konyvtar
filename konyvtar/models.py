b0a26304f04503026294b3bad0cdf31d42ec17a8
fogasl <fogas.l.d@gmail.com>
2013-12-04 12:52:28 +0100

    Admin oldali javítások

--- konyvtar/models.py
+++ konyvtar/models.py
@@ -14,6 +14,11 @@ class Szerzo(models.Model):
     """
     Szerző
     """
+    class Meta:
+        ordering = ['nev']
+        verbose_name = u'Szerző'
+        verbose_name_plural = u'Szerzők'
+
     nev = models.CharField(u'Szerző neve', max_length=100)
     szuletett = models.DateTimeField(u'Született', blank=True, null=True)
     elhunyt = models.DateTimeField(u'Elhunyt', blank=True, null=True)
@@ -27,6 +32,12 @@ class Konyv(models.Model):
     """
     Könyv. Egy könyvnek lehet több szerzője is
     """
+
+    class Meta:
+        ordering = ['cim']
+        verbose_name = u'Könyv'
+        verbose_name_plural = u'Könyvek'
+
     cim = models.CharField(u'Cím', max_length=200)
     leiras = models.CharField(u'Leírás', max_length=500, blank=True, null=True)
     szerzo = models.ManyToManyField(Szerzo)
@@ -42,6 +53,12 @@ class Kolcsonzes(models.Model):
     Egy konkrét kölcsönzés. A felhasználóhoz tartozik: egy felhasználónak lehet több kölcsönzése
     is, ugyanazt a könyvet kikölcsönözheti többször is, több könyvet is kölcsönözhet egyszerre.
     """
+
+    class Meta:
+        ordering = ['-datum']
+        verbose_name = u'Kölcsönzés'
+        verbose_name_plural = u'Kölcsönzések'
+
     felhasznalo = models.ForeignKey(User)
     konyv = models.ManyToManyField(Konyv)
     datum = models.DateTimeField(u'Kölcsönzés dátuma')
@@ -72,6 +89,3 @@ class Kolcsonzes(models.Model):
         return " ".join([self.felhasznalo.last_name, self.felhasznalo.first_name])
 
     felhasznalo_neve.short_description = u'Kölcsönző neve'
-
-    def semmi(self):
-        pass
